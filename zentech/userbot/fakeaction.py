1a03d108193c5979913f1eca1c52974ad2a7cfcf
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-15 02:40:30 +0700

    Create fakeaction.py

---
+++ zentech/userbot/fakeaction.py
@@ -0,0 +1,18 @@
+__MODULE__ = "FakeAction"
+__HELP__ = """
+<b>✅ All User
+<i>
+❧ `.ftyping <seccond>` - Displays a fake typing action
+❧ `.fvideo <seccond>` - Displays the act of recording a fake video
+❧ `.faudio <seccond>` - Displays the action of recording a fake voice
+❧ `.fround <seccond>` - Displays the act of recording a fake video
+❧ `.fphoto <seccond>` - Displays the action of sending a fake photo
+❧ `.fsticker <seccond>` - Displays the action of selecting a fake Sticker
+❧ `.fdocument <seccond>` - Showing fake File sending action
+❧ `.flocation <seccond>` - Showing fake Location Share actions
+❧ `.fgame <seccond>` - Displays playing a Fake game
+❧ `.fcontact <seccond>` - Displays a fake Share Contact action
+❧ `.fstop <seccond>` - Stop fake actions in chat
+❧ `.fscreen <amount>` - Showing fake screenshot actions. (Use in Private Chat)
+</i></b>
+"""
