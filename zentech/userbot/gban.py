4b1b7b4461699cbb6432b7f6cacd3f8e5d9f1ed8
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-15 02:41:07 +0700

    Create gban.py

---
+++ zentech/userbot/gban.py
@@ -0,0 +1,10 @@
+__MODULE__ = "Gban"
+__HELP__ = """
+<b>✅ All User
+<i>
+❧ `.gban` - Globally Ban A User
+❧ `.ungban` - Globally Unban an User
+❧ `.gmute` - Globally Mute A User
+❧ `.ungmute` - Globally Unmute an User
+</i></b>
+"""
