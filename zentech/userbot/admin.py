80a05ada49cd05e6b38b724b0cb554abb761aa68
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-15 02:39:28 +0700

    Create admin.py

---
+++ zentech/userbot/admin.py
@@ -0,0 +1,24 @@
+__MODULE__ = "Admin"
+__HELP__ = """
+<b>✅ Admin | ❌ Member
+<i>
+❧ `.ban` - Ban A User
+❧ `.unban` - Unban A User
+❧ `.kick` - Kick A User
+❧ `.promote` or `.fullpromote` - Make member as admin
+❧ `.demote` - Make admin as member
+❧ `.mute` - Mute A User
+❧ `.unmute` - Unmute A User
+❧ `.pin` - Pin A Message
+❧ `.unpin` - Unpin A Message
+❧ `.setgpic <reply photo>` - Change the group profile photo
+❧ `.startvc` - To Start a voice chat group.
+❧ `.stopvc` - To Stop group voice chat.</i>
+
+👮🏼‍♂️ Information about key groups.
+
+lock type : 
+`msg` | `media` | `stickers` | `polls` | `info`  | `invite` | `webprev` |`pin` | `all`
+
+<i>❧ `.lock <Lock type>` - Lock permissions in the group.</i></b> 
+"""
