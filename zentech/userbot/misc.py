80e96bc9b4e759f72cf8c99ce0ccca6e972f0a29
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-15 02:38:36 +0700

    Create misc.py

---
+++ zentech/userbot/misc.py
@@ -0,0 +1,9 @@
+__MODULE__ = "Misc"
+__HELP__ = """
+<b>✅ All User
+<i>
+❧ `.limit` - Check Limit telegram from @SpamBot.
+❧ `.webshot <link>` - To Screenshot a given web page.
+❧ `.open` - To view the contents of the file as text which is sent as a telegram message.
+</i></b>
+"""
