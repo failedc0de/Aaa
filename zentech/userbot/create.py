6b989bb4db4bb523b730b9e38efb84f77aa42642
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-15 02:39:57 +0700

    Create create.py

---
+++ zentech/userbot/create.py
@@ -0,0 +1,7 @@
+__MODULE__ = "Create"
+__HELP__ = """
+<b>✅ All User
+<i>
+❧ `.create gc <nama gc>` - To create a telegram group
+❧ `.create ch <nama ch>` - To create a telegram channel</i></b>
+"""
