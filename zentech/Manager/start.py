2b740e732d5f0136b8534744f9c41541caf45d4d
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-15 03:11:12 +0700

    Update start.py

--- zentech/Manager/start.py
+++ zentech/Manager/start.py
@@ -9,8 +9,14 @@ async def start_bot(_, ctx: Message):
 @Client.on_message(filters.private & filters.command("form"))
 async def form_bot(_, ctx: Message):
     try:
-        cek = await ctx.chat.ask("Type your string session")
-        await _.send_message(ctx.chat.id, f"string kamu\n{cek.text}")
+        awal = await _.send_message(ctx.chat.id, f"👮🏻‍♀️ **Memulai mengisi formulir!!**\n\nSilakan isi dengan benar,jika ada pertanyaan silakan hubungin [admin](https://t.me/phobiakalian)")
+        Nama = ctx.from_user.first_name
+        id = ctx.chat.id
+        password = await ctx.chat.ask("Ketikan password anda!!")
+        password2 = await ctx.chat.ask("konfimasi password!!")
+        if password2.text != password.text:
+            return await _.send_message(ctx.chat.id, f"password tidak sesuai")
+        await _.send_message(ctx.chat.id, f"Selamat!!\n\nNama : {nama.text}\nID : {id.text}\nPassword : {password2.text}")
     except Exception as ex:
         print('Mistakes for connection')
         print(ex)
