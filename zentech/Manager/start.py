4c6a7ac294d90a1063739929538317bd27d78650
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-17 19:35:49 +0700

    Update start.py

--- zentech/Manager/start.py
+++ zentech/Manager/start.py
@@ -1,5 +1,14 @@
 from pyrogram import Client, idle, filters
 from pyrogram.types import Message
+from zentech.Database.test import get_authuser_names, save_authuser, delete_authuser, extract_user
+
+async def int_to_alpha(user_id: int) -> str:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    text = ""
+    user_id = str(user_id)
+    for i in user_id:
+        text += alphabet[int(i)]
+    return text
 
 
 @Client.on_message(filters.private & filters.command("start"))
@@ -21,7 +30,65 @@ async def form_bot(_, ctx: Message):
         print('Mistakes for connection')
         print(ex)
 
-
+@Client.on_message(filters.command("auth") & filters.group)
+async def auth(_, ctx: Message):
+    chat_id = ctx.chat.id
+    userid = await extract_user(ctx)
+    await ctx.reply_text("auth CMD)
+    if not ctx.reply_to_message:
+        if len(ctx.command) != 2:
+            return await ctx.reply_text("😕 Maaf, **Saya tidak bisa** menemukan pengguna ini!\n\n» coba beri saya id atau username pengguna tersebut")
+        user = ctx.text.split(None, 1)[1]
+        if "@" in user:
+            user = user.replace("@", "")
+        user = await _.get_users(user)
+        user_id = ctx.from_user.id
+        token = await int_to_alpha(user.id)
+        from_user_name = ctx.from_user.first_name
+        from_user_id = ctx.from_user.id
+        _check = await get_authuser_names(ctx.chat.id)
+        count = 0 
+        for smex in _check:
+            count += 1
+        if int(count) == 20:
+            return await ctx.reply_text("😕 Anda hanya dapat memiliki 20 Pengguna dalam daftar pengguna **resmi** grup Anda.")
+        if token not in _check:
+            assis = {
+                "auth_user_id": user.id,
+                "auth_name": user.first_name,
+                "admin_id": from_user_id,
+                "admin_name": from_user_name,
+            }
+            await save_authuser(ctx.chat.id, token, assis)
+            mention = user.mention
+            await ctx.reply_text(f"🧙 **menambahkan** {mention} ke **daftar pengguna resmi**, mulai sekarang dia bisa **menggunakan** perintah **Admin.**")
+            return
+        else:
+            return await ctx.reply_text(f"{mention} sudah ada di **daftar pengguna resmi** dan dia dapat menggunakan **perintah admin.**")
+    from_user_id = ctx.from_user.id
+    user_id = ctx.reply_to_message.from_user.id
+    user_name = ctx.reply_to_message.from_user.first_name
+    token = await int_to_alpha(user_id)
+    from_user_name = ctx.from_user.first_name
+    _check = await get_authuser_names(ctx.chat.id)
+    count = 0
+    for smex in _check:
+        count += 1
+    if int(count) == 20:
+        return await ctx.reply_text("😕 Anda hanya dapat memiliki 20 Pengguna dalam daftar pengguna **resmi** grup Anda.")
+    if token not in _check:
+        assis = {
+            "auth_user_id": user_id,
+            "auth_name": user_name,
+            "admin_id": from_user_id,
+            "admin_name": from_user_name,
+        }
+        await save_authuser(ctx.chat.id, token, assis)
+        await ctx.reply_text(f"🧙 **menambahkan** {ctx.reply_to_message.from_user.mention} ke **daftar pengguna resmi**, mulai sekarang dia bisa **menggunakan** perintah **Admin.**")
+        return
+    else:
+        return await ctx.reply_text(
+            f"{ctx.reply_to_message.from_user.mention} sudah ada di **daftar pengguna resmi** dan dia dapat menggunakan **perintah admin.**")
 
 __MODULE__ = "Misc"
 __HELP__ = """
